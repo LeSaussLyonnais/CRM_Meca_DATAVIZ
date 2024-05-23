6de510e1428f8bb8d85775db28c8a49f730776f1
Thomas MOSSAZ <thomas.mossaz@2024.icam.fr>
2024-05-23 14:37:57 +0200

    Resolution derniers conflits sur le tasks.py et sur le urls.py après merge et verif du fonctionnement du backend validée

--- src/BlogApp/urls.py
+++ src/BlogApp/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 #from django.conf.urls import url
-<<<<<<< HEAD
 from .views import index, endpt_popup_addatelier, endpt_addatelier, TachePDCView
 
 urlpatterns = [
@@ -8,15 +7,4 @@ urlpatterns = [
     path('PopupAjoutAtelier', endpt_popup_addatelier, name="page_popup_ajout_atelier"),
     path('AjoutAtelier', endpt_addatelier, name="page_ajout_atelier"),
     path('Site_<str:nom_site>/Atelier_<str:nom_atelier>/Annee_<str:num_annee>/Semaine_<str:num_semaine>/', TachePDCView.as_view(), name="blog-tache")
-=======
-from .views import *
-
-urlpatterns = [
-    path('', index, name="blog-index"),
-    path('ordo', ordo, name="blog-ordo"),
-    path('charge/', charge, name="charge"),
-    path('chargeUpdate/', endpoint_pdc, name="chargeUpdate"),
-    path('article<str:num_article>/', article, name="blog-article"),
-    path('React', WeatherView.as_view(), name="WeatherReactView")
->>>>>>> dev_front
 ]
